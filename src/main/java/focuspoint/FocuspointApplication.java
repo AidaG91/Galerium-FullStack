9a6ce44aa052b7bd6e9b6a89d14535b3dadc677a
AidaG91 <aida.garmus@gmail.com>
2025-09-04 14:46:21 +0200

    First Commit

---
+++ src/main/java/focuspoint/FocuspointApplication.java
@@ -0,0 +1,13 @@
+package focuspoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocuspointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocuspointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/focuspoint/FocuspointApplicationTests.java
@@ -0,0 +1,13 @@
+package focuspoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocuspointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
